4e2ff927f91c66bb174bf3cb8ab92c99d1fc743e
Abdulrahamn-AL-Sabagh <abdsabagh8@gmail.com>
2025-10-29 20:12:26 +0100

    add rgb and fix bugs in main.v

--- src/main.v
+++ src/main.v
@@ -1,38 +1,37 @@
-
-/*
-A clock divider in Verilog, using the cascading
-flip-flop method.
-*/
-
 module fourConnect(
-  	
 	input clk,
-  input [7:0] pins,
-  	output [2:0] row,
-  	output [2:0] col,
-  output [1:0] final_state
+	input [7:0] pins,
+  	output reg [2:0] row,
+  	output reg [2:0] col,
+	output reg [1:0] final_state
 );
-  
-  
-  reg [1:0]player_state = 2'b_01;
-  reg row_stored = 0;
-  reg [2:0]row_index = 0;
-  reg [2:0]col_index = 0;
-  reg [1:0] board [7:0][7:0];
-  reg row_updated = 0;
-  reg [1:0]state = 2'b00;
 
-  
+  reg [1:0] player_state;
+  reg row_stored;
+  reg [2:0]row_index ;
+  reg [2:0]col_index ;
+  reg [1:0] board [7:0][7:0];
+  reg row_updated;
+  reg [1:0]state;
+
+  initial begin
+	  player_state = 2'b01;
+	  row_stored = 0;
+	  row_index = 0;
+	  col_index = 0;
+	  row_updated = 0;
+	  state = 2'b00;
+  end 
   always @(posedge clk) begin
     if (row_index == 0) begin
   
     case(pins)
-      8'b_0000000: row_index <= 1;
-      8'b_0000001: row_index <= 2;
-      8'b_0000010: row_index <= 3;
-      8'b_0000100: row_index <= 4;
-      8'b_0001000: row_index <= 5;
-      8'b_0010000: row_index <= 6;
+      8'b0000000: row_index <= 1;
+      8'b0000001: row_index <= 2;
+      8'b0000010: row_index <= 3;
+      8'b0000100: row_index <= 4;
+      8'b0001000: row_index <= 5;
+      8'b0010000: row_index <= 6;
       default:
         row_index <= 0;
     	endcase 
@@ -41,13 +40,13 @@ module fourConnect(
     	col_index <= 0;
     if (row_updated == 1 ) begin
       case(pins)
-      8'b_0000000: col_index <= 1;
-      8'b_0000001: col_index <= 2;
-      8'b_0000010: col_index <= 3;
-      8'b_0000100: col_index <= 4;
-      8'b_0001000: col_index <= 5;
-      8'b_0010000: col_index <= 6;
-      8'b_0100000: col_index <= 7;
+      8'b0000000: col_index <= 1;
+      8'b0000001: col_index <= 2;
+      8'b0000010: col_index <= 3;
+      8'b0000100: col_index <= 4;
+      8'b0001000: col_index <= 5;
+      8'b0010000: col_index <= 6;
+      8'b0100000: col_index <= 7;
          default:
            col_index <= 0;
      endcase
@@ -56,9 +55,9 @@ module fourConnect(
 
     if (col_index != 0 && row_index != 0) begin
     
-    if (board[row_index][col_index] == 2'b_00 ) begin 
+    if (board[row_index][col_index] == 2'b00 ) begin 
       board[row_index][col_index] <= player_state;
-	player_state <= player_state ^ 2'b_11;
+	player_state <= player_state ^ 2'b11;
       	row <=  row_index;
     	col <=  col_index; 
       	row_index <= 0;
